02b17a5af698fde7101494aed7650aef5cc14286
ekongg <elk2son@yahoo.com>
2014-04-11 12:35:47 -0400

    Imports and Contructor

--- src/FlagViewer.java
+++ src/FlagViewer.java
@@ -1,4 +1,22 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FlagViewer {
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+import javax.swing.JFrame;
+import javax.swing.SwingConstants;
 
+public class FlagViewer extends JFrame {
+    private static final long serialVersionUID = 1L;
+    
+    public FlagViewer(){
+        
+    }
 }
