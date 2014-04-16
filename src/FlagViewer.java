6e21a7b65ab5cc0cb4d6d8b00cc8a79149164ee5
ekongg <elk2son@yahoo.com>
2014-04-16 14:04:00 -0400

    Eric
    At least I am TRYING to do SOMETHING

--- src/FlagViewer.java
+++ src/FlagViewer.java
@@ -2,9 +2,13 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -20,8 +24,8 @@ import javax.swing.SwingConstants;
 public class FlagViewer extends JFrame {
     
     private static final long serialVersionUID = 1L;
-    private JLabel flagName, flagTotal;
-    private final JButton aboutButton;
+    private JLabel FlagName, FlagTotal;
+    private final JButton aboutButton, searchDropButton;
     private final JTextField searchField;
     private JList<String>flagList;
     private JComboBox box;
@@ -32,12 +36,42 @@ public class FlagViewer extends JFrame {
         searchField = new JTextField(10);
         
         aboutButton = new JButton("About");
-        //Code for About Button Here
+        aboutButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(final ActionEvent ae){
+                
+            }
+        });
+        
+        
+        searchDropButton = new JButton();
+        //Insert Image into button
+        try{
+            Image img = ImageIO.read(getClass().getResource("resources/arrowButton.jpg"));
+            searchDropButton.setIcon(new ImageIcon(img));
+        } catch(IOException ioe){
+        }
+        
+        searchDropButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(final ActionEvent ae){
+                //Method to list the items in the ComboBox 
+            }
+        });
+        
+        
         
         //Populate ArrayList of Flags
         JScrollPane scrollPane = new JScrollPane(flagList);
         
         final JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new GridLayout(2,2));
+        
+        inputPanel.add(new JLabel("Total:"));
+        inputPanel.add(FlagTotal);
+        
+        inputPanel.add(new JLabel("Search:"));
+        inputPanel.add(FlagName);
         
         final JPanel aboutButtonPanel = new JPanel();
         
